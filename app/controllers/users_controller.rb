af7d4348c64428453a30a1242d4902b43eb8f3db
Jaewon Park <eliana.jaewon@gmail.com>
2017-12-20 20:40:08 -0600

    remove update route for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,4 @@ class UsersController < ApplicationController
       render json: {message: user.errors.full_messages}, status: :bad_request
     end 
   end 
-
-  def update
-    user_id = params["id"]
-    user = user.find_by(id: product_id)
-    user.name = params["name"]
-    user.email = params["email"]
-    user.password = params["password"]
-    if user.save
-      render json: {message: "User updated successfully."} 
-    else 
-      render json: {error: user.errors.full_messages}, status: :bad_request
-    end 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
 
   post "/users" => "users#create"
-  patch "/users" => "users#update"
 
   get "/dogs" => "dogs#index"
   get "/dogs/:id" => "dogs#show"
