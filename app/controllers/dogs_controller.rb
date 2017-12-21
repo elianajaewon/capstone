a03c933e719eb0bfb686ef167f72f03fc245077d
Jaewon Park <eliana.jaewon@gmail.com>
2017-12-20 20:39:01 -0600

    update RESTful routes for users and dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,4 +3,10 @@ class DogsController < ApplicationController
     dogs = Dog.all
     render json: dogs.as_json
   end
+
+  def show
+    dog_id = params["id"]
+    dog = Dog.find_by(id: dog_id)
+    render json: dog.as_json
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,22 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation]
       )
     if user.save
-      render json: {message: "New user saved successfuly."}
+      render json: {message: "New user saved successfully."}
     else 
       render json: {message: user.errors.full_messages}, status: :bad_request
     end 
   end 
+
+  def update
+    user_id = params["id"]
+    user = user.find_by(id: product_id)
+    user.name = params["name"]
+    user.email = params["email"]
+    user.password = params["password"]
+    if user.save
+      render json: {message: "User updated successfully."} 
+    else 
+      render json: {error: user.errors.full_messages}, status: :bad_request
+    end 
+  end
 end
